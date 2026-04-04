8d9fe90916e4db1993757321362e76292ed7e093
Jesus carrion <jesusalfonsocarrionmendoza23@gmail.com>
2026-04-04 00:31:00 -0400

    refactor: modularizar lspconfig y optimizar formateo con ruff

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,18 @@
-require("nvchad.configs.lspconfig").defaults()
+local lspconfig = require "lspconfig"
+local nvlsp = require "nvchad.configs.lspconfig"
 
-local servers = { "html", "cssls", "pyright" , "ruff" }
-vim.lsp.enable(servers)
+-- Carga las configuraciones por defecto (on_attach y capabilities)
+nvlsp.defaults()
 
+local servers = { "html", "cssls", "pyright", "ruff" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+        on_attach = nvlsp.on_attach,
+            on_init = nvlsp.on_init,
+                capabilities = nvlsp.capabilities,
+                  }
+                  end
+
+    }
 -- read :h vim.lsp.config for changing options of lsp servers 
