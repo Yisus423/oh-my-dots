9a4e6811cedb7d22dbad98397801725f00e98306
Jesus carrion <jesusalfonsocarrionmendoza23@gmail.com>
2026-04-06 21:21:04 -0400

    tuning(Lsp): transición de Lsp de python de pyright a ty (10 veces más rápido)

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,18 +1,33 @@
 local nvlsp = require "nvchad.configs.lspconfig"
-
--- Carga las configuraciones por defecto (on_attach y capabilities)
 nvlsp.defaults()
 
-local servers = { "html", "cssls", "pyright", "ruff" }
+local servers = { "html", "cssls", "ruff" }
 
+-- Configuración para los servidores estándar
 for _, lsp in ipairs(servers) do
-  -- La nueva API nativa de Neovim 0.11+
   vim.lsp.config(lsp, {
     on_attach = nvlsp.on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
   })
-  
-  -- Ahora es obligatorio habilitar el servidor explícitamente
   vim.lsp.enable(lsp)
 end
+
+-- Configuración específica para TY (Astral)
+-- La API 0.11 necesita saber los filetypes y el root_markers
+vim.lsp.config("ty", {
+  cmd = { "ty", "server" },
+  filetypes = { "python" },
+  root_markers = { "pyproject.toml", "setup.py", ".git", "requirements.txt" },
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    python = {
+      -- Esto busca automáticamente si hay un venv o usa el python3 del sistema
+      pythonPath = vim.fn.glob("./.venv/bin/python") ~= "" and "./.venv/bin/python" or "python3"
+    }
+  }
+})
+
+vim.lsp.enable("ty")
