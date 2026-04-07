e231eb00313807c232203ce3f61c07441f0911bc
Jesus carrion <jesusalfonsocarrionmendoza23@gmail.com>
2026-04-06 21:22:15 -0400

    feature(nvim):agregados relative numbers a mi nvim

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -2,5 +2,7 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+vim.o.number = true          -- Muestra el número de la línea actual
+vim.o.relativenumber = true  -- Muestra los números relativos en las demás
