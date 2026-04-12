88820bc85c72bd72431be42c16791f20a9c56906
Jesus carrion <jesusalfonsocarrionmendoza23@gmail.com>
2026-04-11 21:25:07 -0400

    arreglado check_screen.sh

--- bin/.local/bin/check_screen.sh
+++ bin/.local/bin/check_screen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # script para automatizar la verificación de hdmi en i3 wm
 if xrandr | grep -q "HDMI-1 connected"; then
-  autoarandr --load tv_house
+  autorandr --load tv_house
 else
-  autoarandr --load mobile
+  autorandr --load mobile
 fi
